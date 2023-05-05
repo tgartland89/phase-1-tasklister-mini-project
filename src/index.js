fb5df44cd1357e42792605f70f3bc24137bc8038
tgartland89 <thomas.gartland@gmail.com>
2023-05-05 12:07:03 -0600

    document query slectior to create a submit for new tasks to todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  const form = document.querySelector('#create-task-form')
+
+  form.addEventListener('submit',(event) =>{
+    event.preventDefault()
+    const taskTxt = event.target["new-task-desctiption"].value 
+
+    const list = document.querySelector('#tasks')
+    const newLi = document.createElement('li')
+    newLi.textcontent= taskTxt
+
+    list.appendChild(newLi)
+  })
 });
